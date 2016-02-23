3ef0138a29670636f0cd8a746f8203c84cd68cc5
codybowen <codysbowen@icloud.com>
2016-02-22 16:21:51 -0800

    Added a few tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '/search' => 'pages#search', as: :search
   get '/schedule' => 'users#schedule', as: :schedule
   get '/signup' => 'users#new', as: :signup
-  get '/login' => 'access#login', as: :login
+  get '/login' => 'access#login', as: :login  
   post   'login'   => 'access#attempt_login'
   delete 'logout' => 'access#logout'
   resources :users
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,25 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'User unique' do
+    user = User.new({"username" => "test", "password" => "testing"})
+    user.save
+    user2 = User.new({"username" => "test", "password" => "asadw"})
+    assert_not user2.save, "Saved the user with the same name"
+  end
+  
+  test 'Password blank' do
+    user = User.new({"username" => "test"})
+    assert_not user.save, "Saved the user with no password"
+  end
+  
+  test 'Username blank' do
+    user = User.new({"password" => "testing"})
+    assert_not user.save, "Saved user with no username"
+  end
+  
+  test 'Password less than 5 characters' do
+    user = User.new({"username" => "test", "password" => "test"})
+    assert_not user.save, "Saved user with password < 5 characters"
+  end
 end
