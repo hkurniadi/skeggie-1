63c60c7d6445fc2bb891907a4b6202511a7145a8
codybowen <codysbowen@icloud.com>
2016-03-03 15:02:56 -0800

    Added error message if the query does not have a match in the API, also added ability to filter by course number.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,9 @@
 function process()
 {
   var url="http://localhost:3000/search/" + document.getElementById("semester").value + "/" + document.getElementById("department").value;
+  if (document.getElementById("coursenum").value != null) {
+    url += "/" + document.getElementById("coursenum").value;
+  }
   location.href=url;
   return false;
 }
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/profile/:id' => 'users#profile', as: :profile
   get '/search' => 'pages#search', as: :search
   get '/search/:semester/:subject' => 'pages#search'
+  get '/search/:semester/:subject/:courseid' => 'pages#search'
   get '/schedule' => 'users#schedule', as: :schedule
   get '/signup' => 'users#new', as: :signup
   get '/login' => 'access#login', as: :login  
