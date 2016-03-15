92379b57a8ae7f0f6ecea24a7a576755915f0d04
codybowen <codysbowen@icloud.com>
2016-03-14 23:56:17 -0700

    Added couple tests to users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	test "should return profile search" do
+		get(:profile_search)
+		assert_response :success
+	end
+	
+	test "should return profile search with params" do
+		get(:profile_search, {:search_by => "1", :search_term => "Test"})
+		assert_response :success
+	end
 end
